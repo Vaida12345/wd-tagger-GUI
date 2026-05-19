14f26031038b15f9cde369b17bd2b275090700fa
Vaida <developer@vaidaf.net>
2026-05-19 10:32:23 +1000

    Initial Commit

---
+++ WaifuDiffusion Tagger/WaifuDiffusion_TaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaifuDiffusion_TaggerApp.swift
+//  WaifuDiffusion Tagger
+//
+//  Created by Vaida on 2026-05-19.
+//
+
+import SwiftUI
+
+@main
+struct WaifuDiffusion_TaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
